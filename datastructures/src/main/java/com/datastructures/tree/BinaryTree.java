e9db25c56e7d4cd28472388f3d9a1d3fd7cce8a7
Nagesha <nageshasriram@gmail.com>
2019-01-08 23:10:35 +0530

    Trees

---
+++ datastructures/src/main/java/com/datastructures/tree/BinaryTree.java
@@ -0,0 +1,19 @@
+package com.datastructures.tree;
+
+public class BinaryTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Node{
+	int data;
+	Node left,right;
+	
+	public Node(int data) {
+		this.data = data;
+		left=right=null;
+	}
+}
