b16a9d64cc33112d52cd6e603acc59338df4faad
Nagesha <nageshasriram@gmail.com>
2019-01-08 23:15:23 +0530

    New chages

--- datastructures/src/main/java/com/datastructures/tree/BinaryTree.java
+++ datastructures/src/main/java/com/datastructures/tree/BinaryTree.java
@@ -1,10 +1,21 @@
 package com.datastructures.tree;
 
 public class BinaryTree {
-
+	Node root;
+	
+	BinaryTree(){
+		root = null;
+	}
+	BinaryTree(int data){
+		root = new Node(data);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BinaryTree tree = new BinaryTree();
+		
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
 	}
 
 }
