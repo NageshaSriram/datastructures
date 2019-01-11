637025051c3ab21cbb3734c24ec56e7139a30aaf
Nagesha <nageshasriram@gmail.com>
2019-01-11 12:40:26 +0530

    new changes

--- datastructures/src/main/java/com/datastructures/tree/BinaryTree.java
+++ datastructures/src/main/java/com/datastructures/tree/BinaryTree.java
@@ -28,7 +28,7 @@ public class BinaryTree {
 		preOrder(tree.root);
 
 		System.out.println("Create tree using Array of elements");
-		int[] arr = {25,15,50,10,22,35,70,4,12,18,24,31,44,66,90};
+		int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
 		tree.tree1 = createBinaryTree(arr, tree.tree1, 0);
 		
 		System.out.println("Tree1::");
@@ -39,6 +39,19 @@ public class BinaryTree {
 		inOrder(tree.tree1);
 		System.out.print("\nPre Order::");
 		preOrder(tree.tree1);
+		System.out.println("\nLeavs of Tree1");
+		printLeaves(tree.tree1);
+		
+		System.out.println("\n All left outer Nodes");
+		printLeftNodes(tree.tree1);
+		
+		System.out.println("\n All right outer Nodes");
+		printRightNodes(tree.tree1);
+		
+		printTreeMirror(tree.tree1);
+		System.out.print("\nPost Order::");
+		postOrder(tree.tree1);
+		
 		
 	}
 
@@ -80,7 +93,52 @@ public class BinaryTree {
 		return root;
 		
 	}
-
+	
+	public static void printLeaves(Node root) {
+		if(root == null) {
+			return;
+		}
+		if(root.left == null && root.right == null) {
+			System.out.print(root.data + " ");
+		}
+		printLeaves(root.left);
+		printLeaves(root.right);
+	}
+	
+	public static void printLeftNodes(Node node) {
+		if(node == null) {
+			return;
+		}
+		System.out.print(node.data + " ");
+		if(node.left != null) {
+			printRightNodes(node.left);
+		}else {
+			printRightNodes(node.right);
+		}
+	}
+	public static void printRightNodes(Node node) {
+		if(node == null) {
+			return;
+		}
+		System.out.print(node.data + " ");
+		if(node.right != null) {
+			printRightNodes(node.right);
+		}else {
+			printRightNodes(node.left);
+		}
+	}
+	public static void printTreeMirror(Node node) {
+		if(node == null) {
+			return;
+		}
+		Node temp = node.left;
+		node.left = node.right;
+		node.right = temp;
+		printTreeMirror(node.left);
+		printTreeMirror(node.right);
+		
+		
+	}
 }
 
 class Node {
